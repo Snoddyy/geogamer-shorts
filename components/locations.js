c222446df35811b296678e182840273e46a69a55
Snoddyy <kremer.sebastien@gmail.com>
2025-08-25 17:35:07 +0200

    refactor: update images and locations for new content
    - Renamed and updated image entries in images.js to reflect new character and antagonist categories.
    - Replaced video URLs and image paths to point to the latest resources for characters and maps.
    - Simplified the locations.js file by removing unused entries and updating the panorama image paths for consistency with the new structure.

--- components/locations.js
+++ components/locations.js
@@ -2,152 +2,38 @@
 export const locations = [
   {
     id: 1,
-    name: "Panorama Leo",
+    name: "Panorama",
     videoUrl:
       "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/assets/videos/rules_pano.webm",
     images: [
       {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/panorama/1-1.webp",
+        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e10/20_panorama_1/1-1.webp",
         yaw: 3.2,
         fov: 50.0,
       },
       {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/panorama/1-2.webp",
+        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e10/20_panorama_1/1-2.webp",
         yaw: 6.28,
         fov: 50.0,
       },
       {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/panorama/1-3.webp",
+        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e10/20_panorama_1/1-3.webp",
         yaw: 3.21,
         fov: 50.0,
       },
       {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/panorama/1-4.webp",
+        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e10/20_panorama_1/1-4.webp",
         yaw: 5.04,
         fov: 50.0,
       },
       {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/panorama/1-5.webp",
+        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e10/20_panorama_1/1-5.webp",
         yaw: 0.0,
         fov: 50.0,
       },
       {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/panorama/1-6.webp",
+        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e10/20_panorama_1/1-6.webp",
         yaw: 0.0,
-        fov: 50.0,
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: "Panorama TGR",
-    videoUrl:
-      "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/assets/videos/rules_pano.webm",
-    images: [
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/panorama_tgr/1-1.webp",
-        yaw: 0.0,
-        fov: 50.0,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/panorama_tgr/1-2.webp",
-        yaw: 0.0,
-        fov: 50.0,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/panorama_tgr/1-3.webp",
-        yaw: 0.0,
-        fov: 50.0,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/panorama_tgr/1-4.webp",
-        yaw: 3.38,
-        fov: 50.0,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/panorama_tgr/1-5.webp",
-        yaw: 3.14,
-        fov: 54.9,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/panorama_tgr/1-6.webp",
-        yaw: 0.0,
-        fov: 50.0,
-      },
-    ],
-  },
-  {
-    id: 3,
-    name: "DS3-ER",
-    videoUrl:
-      "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/assets/videos/rules_ds3.webm",
-    images: [
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/DS3-ER/1-1.webp",
-        yaw: 5.39,
-        fov: 57.68,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/DS3-ER/1-2.webp",
-        yaw: 3.11,
-        fov: 50.0,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/DS3-ER/1-3.webp",
-        yaw: 1.23,
-        fov: 50.0,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/DS3-ER/1-4.webp",
-        yaw: 3.18,
-        fov: 50.0,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/DS3-ER/1-5.webp",
-        yaw: 6.15,
-        fov: 50.0,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/DS3-ER/1-6.webp",
-        yaw: 1.14,
-        fov: 50.0,
-      },
-    ],
-  },
-  {
-    id: 4,
-    name: "Stanley",
-    videoUrl:
-      "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/assets/videos/rules_stanley.webm",
-    images: [
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/stanley/1-1.webp",
-        yaw: 4.7,
-        fov: 50.0,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/stanley/1-2.webp",
-        yaw: 0.0,
-        fov: 50.0,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/stanley/1-3.webp",
-        yaw: 4.71,
-        fov: 50.0,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/stanley/1-4.webp",
-        yaw: 0.0,
-        fov: 50.0,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/stanley/1-5.webp",
-        yaw: 0.0,
-        fov: 50.0,
-      },
-      {
-        url: "https://red-bull-checkpoint.s3.eu-west-3.amazonaws.com/geogamer-shorts/e08-e09-shorts/stanley/1-6.webp",
-        yaw: 6.22,
         fov: 50.0,
       },
     ],
